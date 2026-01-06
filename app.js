0cabb623d8cc0b3d079d6203865311bc35ccea9a
ZalanMede <medezalan@turr.hu>
2026-01-06 08:59:34 +0100

    SCRUM-15 Login/Register switching integration, background positioning

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
+document.getElementById("flipID").addEventListener("click", function (e) {
+    console.log("before flip");
+    document.getElementsByClassName("flipCardInner")[0].style.transform = "rotateY("+180+"deg)";
+    logRegFlipBool = true;
+    console.log("flip complete");
+});
+
+document.getElementById("flipBackID").addEventListener("click", function (e) {
+  console.log("before flip back");
+  document.getElementsByClassName("flipCardInner")[0].style.transform = "rotateY("+0+"deg)";
+  logRegFlipBool = true;
+  console.log("flip back complete");
+});
