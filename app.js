7c694e8af035af6c04117b7e67c36abf4bbeb707
horvathdanielgabor <horvathdanielgabor@turr.hu>
2026-01-15 14:15:15 +0100

    pHone veiw Header button actions

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+function headerDropDown(myBlock, offBlock) {
+  var x = document.getElementById(myBlock);
+  if (x.style.display === "block") {
+    x.style.display = "none";
+    document.getElementById("home").style.display = "block"
+  } else {
+    x.style.display = "block";
+    document.getElementById("home").style.display = "none"
+    document.getElementById(offBlock).style.display = "none"
+  }
+}
+
+
+
 document.getElementById("flipID").addEventListener("click", function (e) {
     console.log("before flip");
     document.getElementsByClassName("flipCardInner")[0].style.transform = "rotateY("+180+"deg)";
