cde31d91063f3c4c0cede4c15ad07b8e3a41dfbd
ZalanMede <medezalan@turr.hu>
2026-01-15 14:10:11 +0100

    SCRUM-15 Flip animáció kész, login struktúra változtatva

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 document.getElementById("flipID").addEventListener("click", function (e) {
     console.log("before flip");
 
-    document.getElementsByClassName("flipCardFront")[0].style.animation = "cardFlipFade 1s 1s forwards";
+    document.getElementsByClassName("flipCardFront")[0].style.animation = "cardFlipFade 0.3s 0.3s forwards";
 
     setTimeout(() => {
-      document.getElementsByClassName("flipCardFront")[0].style.background = "blue";
-    }, 1000);
+      document.getElementsByClassName("flipCardFront")[0].style.background = "#53629E";
+    }, 300);
 
     setTimeout(() => {
-      document.getElementsByClassName("flipCardBack")[0].style.background = "blue";
+      document.getElementsByClassName("flipCardBack")[0].style.display = "flex";
+      document.getElementsByClassName("flipCardBack")[0].style.background = "#53629E";
+      document.getElementsByClassName("flipCardBack")[0].style.color = "white";
       document.getElementsByClassName("flipCardInner")[0].style.transform = "rotateY("+180+"deg)";
       document.getElementsByClassName("flipCardFront")[0].style.background = "none";
       setTimeout(() => {
         document.getElementsByClassName("flipCardFront")[0].style.display = "none";
-        document.getElementsByClassName("flipCardBack")[0].style.animation = "cardFlipFadeBack 1s 1s forwards";
+        document.getElementsByClassName("flipCardBack")[0].style.animation = "cardFlipFadeBack 0.3s 0.3s forwards";
 
         setTimeout(() => {
           document.getElementsByClassName("flipCardBack")[0].style.background = "rgba(255,255,255,0.3)";
@@ -22,28 +24,30 @@ document.getElementById("flipID").addEventListener("click", function (e) {
           document.getElementsByClassName("flipCardBack")[0].style.animation = "none";
 
           console.log("flip complete");
-        }, 2000)
-      }, 1000)
-    }, 2000);
+        }, 500)
+      }, 500)
+    }, 500);
 });
 
 document.getElementById("flipBackID").addEventListener("click", function (e) {
   console.log("before flip back");
 
-  document.getElementsByClassName("flipCardBack")[0].style.animation = "cardFlipFade 1s 1s forwards";
+  document.getElementsByClassName("flipCardBack")[0].style.animation = "cardFlipFade 0.3s 0.3s forwards";
 
     setTimeout(() => {
-      document.getElementsByClassName("flipCardBack")[0].style.background = "blue";
-    }, 1000);
+      document.getElementsByClassName("flipCardBack")[0].style.background = "#53629E";
+    }, 300);
 
     setTimeout(() => {
-      document.getElementsByClassName("flipCardFront")[0].style.background = "blue";
+      document.getElementsByClassName("flipCardFront")[0].style.display = "flex";
+      document.getElementsByClassName("flipCardFront")[0].style.background = "#53629E";
+      document.getElementsByClassName("flipCardFront")[0].style.color = "white"; 
       document.getElementsByClassName("flipCardInner")[0].style.transform = "rotateY("+0+"deg)";
       document.getElementsByClassName("flipCardBack")[0].style.background = "none";
       document.getElementsByClassName("flipCardFront")[0].style.display = "flex";
       setTimeout(() => {
         document.getElementsByClassName("flipCardBack")[0].style.display = "none";
-        document.getElementsByClassName("flipCardFront")[0].style.animation = "cardFlipFadeBack 1s 1s forwards";
+        document.getElementsByClassName("flipCardFront")[0].style.animation = "cardFlipFadeBack 0.3s 0.3s forwards";
 
         setTimeout(() => {
           document.getElementsByClassName("flipCardFront")[0].style.background = "rgba(255,255,255,0.3)";
@@ -52,7 +56,7 @@ document.getElementById("flipBackID").addEventListener("click", function (e) {
           document.getElementsByClassName("flipCardFront")[0].style.animation = "none";
           
           console.log("flip back complete");
-        }, 2000)
-      }, 1000)
-    }, 2000);
+        }, 500)
+      }, 500)
+    }, 500);
 });
