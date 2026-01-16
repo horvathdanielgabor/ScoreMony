5b9bc936c99dc29b94ec3d927039ca20c7ba169f
horvathdanielgabor <horvathdanielgabor@turr.hu>
2026-01-16 13:28:29 +0100

    SCRUM-13 Phone view Header resp

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 function headerDropDown(myBlock, offBlock) {
   var x = document.getElementById(myBlock);
-  if (x.style.display === "block") {
-    x.style.display = "none";
-    document.getElementById("home").style.display = "block"
+  if (x.style.display === "flex") {
+    x.style.display = null;
+    document.getElementById("home").style.display = "flex";
+    document.getElementById(offBlock).style.display = null;
+    document.getElementsByClassName(offBlock)[0].style.display = null;
   } else {
-    x.style.display = "block";
-    document.getElementById("home").style.display = "none"
-    document.getElementById(offBlock).style.display = "none"
+    x.style.display = "flex";
+    document.getElementById("home").style.display = "none";
+    document.getElementById(offBlock).style.display = "none";
+    document.getElementsByClassName(offBlock)[0].style.display = "none";
   }
 }
+
+window.addEventListener("resize", function(){
+  for (let element of document.getElementsByClassName("head"))
+  {
+    element.style.display = null
+  };
+});
 
 
 
