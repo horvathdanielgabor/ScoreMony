751dc4501cff6d9a3507ea0fd508382fc5e4b27a
horvathdanielgabor <123636432+horvathdanielgabor@users.noreply.github.com>
2026-01-27 09:46:48 +0000

    SCRUM-13 responsive header finished and ready to test

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
+var containOriginal;
+
 function headerDropDown(myBlock, offBlock) {
-  var x = document.getElementById(myBlock);
-  if (x.style.display === "flex") {
-    x.style.display = null;
+  var turnOn = document.getElementById(myBlock);
+  var change1 = document.getElementsByClassName(myBlock)[0].children[0];
+  var turnOff = document.getElementById(offBlock);
+  var change2 = document.getElementsByClassName(offBlock)[0];
+  if (turnOn.style.display === "flex") {
+    turnOn.style.display = null;
+    change1.src = containOriginal;
     document.getElementById("home").style.display = "flex";
-    document.getElementById(offBlock).style.display = null;
-    document.getElementsByClassName(offBlock)[0].style.display = null;
+    turnOff.style.display = null;
+    change2.style.display = null;
   } else {
-    x.style.display = "flex";
+    turnOn.style.display = "flex";
+    containOriginal = change1.src;
+    change1.src = "images/CloseIcon.png";
     document.getElementById("home").style.display = "none";
-    document.getElementById(offBlock).style.display = "none";
-    document.getElementsByClassName(offBlock)[0].style.display = "none";
+    turnOff.style.display = "none";
+    change2.style.display = "none";
   }
 }
 
