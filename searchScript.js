3180b8d5d51997d79a8907d192591c2570e81347
ZalanMede <medezalan@turr.hu>
2026-03-03 08:45:14 +0100

    SCRUM-17 Filterek gomb és összecsukás kész, reszponzivitás elkezdve

--- searchScript.js
+++ searchScript.js
@@ -3,12 +3,14 @@ function filterDropDownTrigger(el){
     console.log(holderParent);
     if (!el.classList.contains("spinEffect")) {
         el.classList.add("spinEffect");
+        holderParent.style.maxHeight = 0;
         setTimeout(() => {
             el.style.transform = "rotate(-90deg)";
         }, 500)
     }
     else{
         el.classList.remove("spinEffect");
+        holderParent.style.maxHeight = holderParent.scrollHeight + "px";
         setTimeout(() => {
             el.classList.add("spinEffectBack");
         }, 10)
