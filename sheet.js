93e4f90664a6ac1e39a2a344c0675c3e4a77f754
horvathdanielgabor <horvathdanielgabor@turr.hu>
2026-04-21 08:46:01 +0200

    SCRUM-18 Kiválasztott kotta javascript betöltése folyamatban

--- sheet.js
+++ sheet.js
@@ -3,19 +3,31 @@ let details = document.getElementById("details");
 let description = document.getElementById("description");
 let collector = [];
 
-async function loadIn(SheetSoucre, Details, Description) {
+async function loadIn(SheetSoucreDetails, Details, Description) {
+    collector = []
+    let fileName = SheetSoucreDetails.splice(-2,1)[0];
+    let fileType = SheetSoucreDetails.splice(-1)[0];
+    let path = "IKT_kották\\" + SheetSoucreDetails.reduce((accumulator, item) => accumulator + "\\" + item);
+    console.log(path);
+    console.log(fileName);
     do
     {
         try
         {
-
+            
         }
         catch
         {
             
         }
     }
-    while (condition)
+    while (collector.length)
+
+    collector.forEach((data) => {
+        let img = document.createElement();
+        img.src = path + "\\Képek\\" + fileName + "\\" + fileName + fileType;
+        sheet.append()
+    });
     
     for (let i = 0; i < Details.length; i++) {
         if (/\w{2,}/.test(Details[i]))
@@ -23,10 +35,9 @@ async function loadIn(SheetSoucre, Details, Description) {
             details.children[i].children[1].innerText = Details[i];
             console.log(Details[i]);
         }
-        
     }
 
     description.children[0].innerText = await Description;
 }
 
-loadIn("Love_for_sale.jpg",["da","da","da","da","da","Az micsoda?"],"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum modi, atque laborum similique quas laboriosam adipisci illum quaerat, aperiam odio ut repellat id optio corrupti deleniti impedit quidem nisi officia. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum modi, atque laborum similique quas laboriosam adipisci illum quaerat, aperiam odio ut repellat id optio corrupti deleniti impedit quidem nisi officia. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum modi, atque laborum similique quas laboriosam adipisci illum quaerat, aperiam odio ut repellat id optio corrupti deleniti impedit quidem nisi officia. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum modi, atque laborum similique quas laboriosam adipisci illum quaerat, aperiam odio ut repellat id optio corrupti deleniti impedit quidem nisi officia. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum modi, atque laborum similique quas laboriosam adipisci illum quaerat, aperiam odio ut repellat id optio corrupti deleniti impedit quidem nisi officia. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum modi, atque laborum similique quas laboriosam adipisci illum quaerat, aperiam odio ut repellat id optio corrupti deleniti impedit quidem nisi officia.")
+loadIn(["Gitár","Rock","Haladó","Love_for_sale",".jpg"],["da","da","da","da","da","Az micsoda?"],"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum modi, atque laborum similique quas laboriosam adipisci illum quaerat, aperiam odio ut repellat id optio corrupti deleniti impedit quidem nisi officia. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum modi, atque laborum similique quas laboriosam adipisci illum quaerat, aperiam odio ut repellat id optio corrupti deleniti impedit quidem nisi officia. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum modi, atque laborum similique quas laboriosam adipisci illum quaerat, aperiam odio ut repellat id optio corrupti deleniti impedit quidem nisi officia. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum modi, atque laborum similique quas laboriosam adipisci illum quaerat, aperiam odio ut repellat id optio corrupti deleniti impedit quidem nisi officia. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum modi, atque laborum similique quas laboriosam adipisci illum quaerat, aperiam odio ut repellat id optio corrupti deleniti impedit quidem nisi officia. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum modi, atque laborum similique quas laboriosam adipisci illum quaerat, aperiam odio ut repellat id optio corrupti deleniti impedit quidem nisi officia.")
