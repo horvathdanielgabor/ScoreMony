3c37a5af37f9f5a75e712642941e64253d2fa67a
horvathdanielgabor <horvathdanielgabor@turr.hu>
2026-05-11 07:44:59 +0200

    SCRUM-18 metronom funkció elkezdése

--- sheet.js
+++ sheet.js
@@ -3,6 +3,9 @@ let details = document.getElementById("details");
 let description = document.getElementById("description");
 let collector = [];
 
+let toggleM = document.getElementById("metronomeToggle");
+let metronom = null;
+
 async function loadIn(Details, Description) {
     //let path = "IKT_kották\\" + Details.filter().reduce((accumulator, item) => accumulator + "\\" + item);
 
@@ -34,6 +37,34 @@ async function loadIn(Details, Description) {
     }
 
     //description.children[0].innerText = await Description;
+}
+
+async function beat(){
+    if (metronom == null)
+    {
+        break;
+    }
+
+    await setTimeout(()=>{console.log("beat",+document.getElementById("metronomeSpeedSlider").value)},5000);
+    toggleM.style.boxShadow = "0px 0px 0px 5px rgba(0, 0, 0, 0.5)";
+    await setTimeout(()=>{console.log("beat",(600000/+document.getElementById("metronomeSpeedSlider").value))},5000);
+    toggleM.style.boxShadow = null;
+    metronom = setTimeout(()=> {beat()},(600000/+document.getElementById("metronomeSpeedSlider").value));
+}
+
+function metronomeFlipflop(play){
+    if (play)
+    {
+        metronom = setTimeout(()=> {beat()},(600000/+document.getElementById("metronomeSpeedSlider").value));
+        toggleM.setAttribute("onclick","metronomeFlipflop(false)");
+        toggleM.children[0].src = "images/pause.svg";
+    }
+    else
+    {
+        clearTimeout(metronom);
+        toggleM.setAttribute("onclick","metronomeFlipflop(true)");
+        toggleM.children[0].src = "images/start.svg";
+    }
 }
 
 loadIn(["Név","Stílus","Zeneszerző","Hangszer","Kulcs","Kezdő"])
