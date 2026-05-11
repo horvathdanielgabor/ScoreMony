08cffc3d7325c8fffe6ec9ce3250d7890979c84a
horvathdanielgabor <horvathdanielgabor@turr.hu>
2026-05-11 23:41:09 +0200

    SCRUM-18 Kotta nézet oldal pdf betöltése és letöltés gomb kész. Metronom folyamatban.

--- sheet.js
+++ sheet.js
@@ -1,13 +1,24 @@
 let sheet = document.getElementById("sheet");
 let details = document.getElementById("details");
-let description = document.getElementById("description");
-let collector = [];
-let imageSource = null;
 
 let toggleM = document.getElementById("metronomeToggle");
-let metronom = null;
+let metronome = null;
+let isPlaying = false;
+let bpm = document.getElementById('metronomeSpeedSlider').value;
+const slider = document.getElementById('metronomeSpeedSlider');
 
-function loadIn(Details, pfLink) {
+slider.addEventListener('input', () => {
+  bpm = parseInt(slider.value);
+  document.getElementById('metronomeSpeedSlider').textContent = bpm;
+
+  if (isPlaying) {
+    clearInterval(metronome);
+    const intervalMs = (60 / bpm) * 1000;
+    metronome = setInterval(tick, intervalMs);
+  }
+});
+
+async function loadIn(Details, pdfLink) {
     let detailsObject = JSON.parse(Details);
     console.log(detailsObject);
     
@@ -20,54 +31,55 @@ function loadIn(Details, pfLink) {
             }
             catch
             {
-                console.log(detailsObject[key])
-                imageSource = detailsObject[key]
             }
         }
     }
 
-    do
-    {
-        console.log(imageSource);
-        try
-        {
-            let newImage = document.createElement("img");
-            newImage.src = imageSource.;
-            console.log(newImage);
-            sheet.appendChild(newImage);
-        }
-        catch
-        {
-            
-        }
-    }
-    while (collector.length)
+    sheet.innerText = null;
+
+    const pdf = await pdfjsLib.getDocument(pdfLink).promise;
+    console.log(pdf);
+
+      for (let pageNum = 1; pageNum <= pdf.numPages; pageNum++) {
+        const page = await pdf.getPage(pageNum);
+        const viewport = page.getViewport({ scale: 1.5 });
+
+        const canvas = document.createElement('canvas');
+        canvas.width = viewport.width;
+        canvas.height = viewport.height;
+        sheet.appendChild(canvas);
+
+        await page.render({
+          canvasContext: canvas.getContext('2d'),
+          viewport
+        }).promise;
+      }
+
+      document.getElementById("downloadPDF").href = pdfLink;
 }
 
-async function beat(){
-    if (metronom != null)
-    {
-        console.log("beat",+document.getElementById("metronomeSpeedSlider").value);
-        toggleM.style.boxShadow = "0px 0px 0px 5px rgba(0, 0, 0, 0.5)";
-        await setTimeout(()=>{console.log("beat",(600000/+document.getElementById("metronomeSpeedSlider").value))},10000);
-        toggleM.style.boxShadow = null;
-        metronom = setTimeout(()=> {beat()},(600000/+document.getElementById("metronomeSpeedSlider").value));
-    }
+function tick() {
+  toggleM.style.backgroundColor = 'rgba(77, 75, 136, 0.5)';
+  toggleM.style.borderColor = 'rgba(77, 75, 136, 0.5)';
+  setTimeout(() => {
+    toggleM.style.backgroundColor = null;
+    toggleM.style.borderColor = null;
+  }, 80);
 }
 
-function metronomeFlipflop(play){
-    if (play)
+function metronomeFlipflop(){
+    if (!isPlaying)
     {
-        metronom = setTimeout(()=> {beat()},(600000/+document.getElementById("metronomeSpeedSlider").value));
-        toggleM.setAttribute("onclick","metronomeFlipflop(false)");
+        metronome = setInterval(tick,(60/+document.getElementById("metronomeSpeedSlider").value)*1000);
         toggleM.children[0].src = "images/pause.svg";
+        isPlaying = true;
     }
     else
     {
-        clearTimeout(metronom);
-        toggleM.setAttribute("onclick","metronomeFlipflop(true)");
+        clearInterval(metronome);
         toggleM.children[0].src = "images/start.svg";
+        isPlaying = false;
     }
 }
 
-loadIn(localStorage.selectedScore)
+loadIn(localStorage.selectedScore,"IKT_kották/Dob/Pop/Középhaladó/PDF/rolling_in_the_deep.pdf")
